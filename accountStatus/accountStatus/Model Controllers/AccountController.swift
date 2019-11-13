a16f49318bb6754fe7f542f89579b2303a7047e3
RYAN GREENBURG <ryangreenburg@RYANs-MacBook-Pro-2.local>
2019-11-13 16:40:25 -0700

    Initial commit

---
+++ accountStatus/accountStatus/Model Controllers/AccountController.swift
@@ -0,0 +1,9 @@
+//
+//  AccountController.swift
+//  accountStatus
+//
+//  Created by RYAN GREENBURG on 11/13/19.
+//  Copyright © 2019 RYAN GREENBURG. All rights reserved.
+//
+
+import Foundation
---
+++ accountStatus/accountStatus/Models/Account.swift
@@ -0,0 +1,13 @@
+//
+//  Account.swift
+//  accountStatus
+//
+//  Created by RYAN GREENBURG on 11/13/19.
+//  Copyright © 2019 RYAN GREENBURG. All rights reserved.
+//
+
+import Foundation
+
+struct Account {
+    
+}
